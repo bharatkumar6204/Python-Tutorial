66f4ada6504baa5a2484481c485b69e87c9ab423
bharatkumar6204 <bbharatkusavansi@gmail.com>
2026-05-23 01:17:49 +0530

    oops1

--- oops1/code.py
+++ oops1/code.py
@@ -1,3 +1,4 @@
+# 1. Class aur Object
 # class
 class Student:
     name = "bharat"
@@ -5,3 +6,26 @@ class Student:
 # object
 s1 = Student()
 print(s1.name)
+
+
+# 2. Constructor (init)
+
+class Student:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+s2 = Student("bharat",23)
+print(s2.name, s2.age)
+
+# 3. Inheritance
+class Father:
+    def house(self):
+        print("Father has house")
+
+class Son(Father):
+    def Son(self):
+        print("Son has Car")
+s = Son()
+
+s.house()
+s.Son()
