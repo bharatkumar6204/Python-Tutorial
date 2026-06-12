3ed555f5f9d7ed53543afd0eec0837718a0cb099
bharatkumar6204 <bbharatkusavansi@gmail.com>
2026-06-12 22:36:31 +0530

    coding

--- coding/code.py
+++ coding/code.py
@@ -1,8 +1,16 @@
-# print total number of sum
+# remove duplicat formm sorted arr
 
-lst = [1,2,3,4,5]
-total = 0
+def removeDuplicate(nums):
+    if not nums:
+        return 0
 
-for num in lst:
-    total += num
-print(total)
+    i = 0
+    for j in range(1, len(nums)):
+        if nums[i] != nums[j]:
+            i += 1
+            nums[i] = nums[j]
+    return i+1
+nums = [1,1,2,2,3,3]
+k = removeDuplicate(nums)
+print(k)
+print(nums[:k]) 
